bd4fb32d258b6664bfc0225748e527222ecffdd3
maru <porutomaru@gmail.com>
2025-11-30 12:26:27 +0900

    feat: install plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
 vim.g.have_nerd_font = false
-
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -11,15 +9,11 @@ vim.schedule(function()
 end)
 
 vim.o.breakindent = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 vim.o.signcolumn = "yes"
-
 vim.o.cursorline = true
 vim.o.scrolloff = 10
-
 vim.o.confirm = true
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -32,3 +26,32 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end
 })
 
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
+end
+
+--@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+  "NMAC427/guess-indent.nvim",
+
+  {
+    "jpalardy/vim-slime",
+    init = function()
+      vim.g.slime_bracketed_paste = 1
+      vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+      vim.g.slime_dont_ask_default = 1
+      vim.g.slime_target = "tmux"
+    end
+  }
+})
